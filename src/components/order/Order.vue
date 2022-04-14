b373823da3b3f85973a9d88dff517a1889275a90
小孤寡 <1605817176@qq.com>
2022-04-14 16:50:41 +0800

    2022-4-14失误回溯

---
+++ src/components/order/Order.vue
@@ -0,0 +1,75 @@
+<template>
+  <div>
+    <!-- 面包屑导航区 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>订单管理</el-breadcrumb-item>
+      <el-breadcrumb-item>订单列表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <!-- 卡片视图区 -->
+    <el-card>
+      <el-row>
+        <el-col :span="8">
+          <el-input placeholder="请输入内容">
+            <el-button
+              slot="append"
+              icon="el-icon-search"
+            ></el-button></el-input
+        ></el-col>
+      </el-row>
+
+      <!-- 订单列表数据 -->
+      <el-table :data="orderList" border stripe>
+        <el-table-column type="index"></el-table-column>
+        <el-table-column label="订单编号" prop="order_number"></el-table-column>
+        <el-table-column label="订单价格" prop="order_price"></el-table-column>
+        <el-table-column label="是否付款" prop="pay_status"></el-table-column>
+        <el-table-column label="是否发货" prop="is_send"></el-table-column>
+        <el-table-column label="下单时间" prop="create_time">
+          <template slot-scope="scope">
+            {{ scope.row.create_time }}
+          </template>
+        </el-table-column>
+        <el-table-column label="操作">
+          <template>
+            <el-button type="primary" icon="el-icon-edit"></el-button>
+            <el-button type="success" icon="el-icon-location"></el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      // 查询数据对象
+      queryInfo: {
+        query: '',
+        pagenum: 1,
+        pagesize: 10,
+      },
+      total: 0,
+      orderList: [],
+    }
+  },
+  created() {
+    this.getOrderList()
+  },
+  methods: {
+    async getOrderList() {
+      const { data: res } = await this.$http.get('orders', {
+        params: this.queryInfo,
+      })
+      if (res.meta.status !== 200) {
+        return this.$message.error('获取订单列表失败~')
+      }
+      this.total = res.data.total
+      this.orderList = res.data.goods
+    },
+  },
+}
+</script>
+<style lang="less" scoped></style>
